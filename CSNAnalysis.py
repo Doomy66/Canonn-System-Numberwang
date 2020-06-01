80b602f82d4c83dfd24b347a5a6adc12feba544e
Doomy66 <steven.burrows@gmail.com>
2020-06-01 22:46:47 +0100

    Add files via upload
    Cache now works on updated_at
    /new now only referes to WebHook to do a full report, or just messages since last run

--- CSNAnalysis.py
+++ CSNAnalysis.py
@@ -21,6 +21,7 @@ LOCAL_OVERRIDE = False  # Use a local Override file or look up the Canonn Overri
 FACTION_CACHE = []  # Yeah, bad code I know, should have a Cache Class
 NREQ = 0
 
+
 def external_system(system_name):
     global NREQ
     url = 'https://elitebgs.app/api/ebgs/v4/systems'
@@ -31,7 +32,7 @@ def external_system(system_name):
     except:
         print(f'!System Not found : {system_name}')
         return(None)
-    
+
     # consistent nameing between objects
     myload["system_name"] = myload["name"]
     NREQ += 1
@@ -55,42 +56,58 @@ def getfactiondata(name):
     return(x)
 
 
-def BGSDownloader():
+def BGSDownloader(argv):
     global NREQ
     print(f'Processing BGS Data for {factionname}')
+
+    myfile = 'Faction.json'
+    if not(os.path.isfile(myfile)):
+        oldsl = []
+    else:
+        with open(myfile, 'r') as io:
+            oldsl = json.load(io)
 
     # List of all systems the faction has a presence in.
     # This is the master structure that has additional data attached used for processing later
     faction_systems = getfactiondata(factionname)
 
     # Loop through all the systems that the faction is present in
-    for x in range(len(faction_systems)):
+    for sys in faction_systems:
+
         # get full data on the system. contaings xyz and other factions
-        sys = faction_systems[x]
         print(sys['system_name'])
-        url = 'https://elitebgs.app/api/ebgs/v4/systems'
-        payload = {'name': faction_systems[x]['system_name']}
-        r = requests.get(url, params=payload)
-        myload = json.loads(r.content)["docs"][0]
-        inflist = myload["factions"]
-        NREQ += 1
+        # Previous Run
+        olds = list(
+            filter(lambda o: o['system_name'] == sys['system_name'], oldsl))
+
+        if len(olds) == 1 and olds[0]['updated_at'] == sys['updated_at']:  # No Change
+            myload = olds[0]
+            inflist = olds[0]['inflist']
+        else:  # Reread entire system
+            NREQ += 1
+            url = 'https://elitebgs.app/api/ebgs/v4/systems'
+            payload = {'name': sys['system_name']}
+            r = requests.get(url, params=payload)
+            myload = json.loads(r.content)["docs"][0]
+            inflist = myload["factions"]
+            # Loop through all the factions that are present in the system
+            for y in range(len(inflist)):
+                thisf = inflist[y]
+                print('.'+thisf["name"])
+                ffaction = getfactiondata(thisf["name"])
+                # get other factions data to find influence and states
+                ffactionsys = list(
+                    filter(lambda x: x['system_name'] == sys["system_name"], ffaction))[0]
+                # copy other faction influence data
+                thisf["influence"] = ffactionsys["influence"]*100
+                thisf["active_states"] = ffactionsys["active_states"]
+                thisf["conflicts"] = ffactionsys["conflicts"]
+                thisf["pending_states"] = ffactionsys["pending_states"]
+
         # write x,y,z to parent
         sys["x"] = myload["x"]
         sys["y"] = myload["y"]
         sys["z"] = myload["z"]
-        # Loop through all the factions that are present in the system
-        for y in range(len(inflist)):
-            thisf = inflist[y]
-            print('.'+thisf["name"])
-            ffaction = getfactiondata(thisf["name"])
-            # get other factions data to find influence and states
-            ffactionsys = list(
-                filter(lambda x: x['system_name'] == sys["system_name"], ffaction))[0]
-            # copy other faction influence data
-            thisf["influence"] = ffactionsys["influence"]*100
-            thisf["active_states"] = ffactionsys["active_states"]
-            thisf["conflicts"] = ffactionsys["conflicts"]
-            thisf["pending_states"] = ffactionsys["pending_states"]
 
         # inflist should now be full of all factions and their values for this system and can be added to the main structure
         sys["inflist"] = inflist
@@ -103,10 +120,11 @@ def BGSDownloader():
         json.dump(faction_systems, io)
 
     print('Base Date Generated')
+    return faction_systems
 
 
 def Misson_Gen(argv):
-    #print(argv)
+    # print(argv)
     if "/?" in argv:
         print("Canonn System Numberwang:")
         print(" /new = Force new data")
@@ -114,17 +132,8 @@ def Misson_Gen(argv):
         print(" /wait = Pause console at end to review output")
         quit
 
-    stat_run = datetime.now()
-    last_run = None
+    faction_systems = BGSDownloader(argv)
 
-    myfile = 'Faction.json'
-    if ('/new') in argv or not(os.path.isfile(myfile)):
-        BGSDownloader()
-
-    with open(myfile, 'r') as io:
-        faction_systems = json.load(io)
-
-    
     if LOCAL_OVERRIDE:
         # Local Overrides
         oridefile = f'{factionname}Overrides.csv'
@@ -147,22 +156,21 @@ def Misson_Gen(argv):
     except:
         oldmessage = []
 
-
     messages = []
     active_states = []
     pending_states = []
     recovering_states = []
 
-    dIcons = {"war": '<:EliteEagle:231083794700959744> ' , 
-        "election": ':ballot_box: ', 
-        "civilwar": '<:EliteEagle:231083794700959744> ',
-        "override": '<:canonn:231835416548999168> ',
-        "push": '<:Salute2:500679418692501506> ',
-        "data": ':eyes: ',
-        "infgap": ':dagger: ',
-        "mininf": ':chart_with_downwards_trend: ',
-        "info": ':information_source: ',
-        "end": ':clap: ' }
+    dIcons = {"war": '<:EliteEagle:231083794700959744> ',
+              "election": ':ballot_box: ',
+              "civilwar": '<:EliteEagle:231083794700959744> ',
+              "override": '<:canonn:231835416548999168> ',
+              "push": '<:Salute2:500679418692501506> ',
+              "data": ':eyes: ',
+              "infgap": ':dagger: ',
+              "mininf": ':chart_with_downwards_trend: ',
+              "info": ':information_source: ',
+              "end": ':clap: '}
 
     print(f'CSN Missions:')
     # Create a single Message for each faction system
@@ -173,74 +181,75 @@ def Misson_Gen(argv):
         updated = EliteBGSDateTime(sys["updated_at"])
         age = datetime.now() - updated
         oride = list(filter(lambda x: x[0] == sys["system_name"], orides))
-        
 
         # Single Message per sysme for Patrol
         if len(oride) > 0:  # OVERRIDE!
             messages.append(
                 amessage(sys, oride[0][1], oride[0][2]+'*',
-                dIcons['override'] if oride[0][3] == '' else dIcons[oride[0][3]]))
+                         dIcons['override'] if oride[0][3] == '' else dIcons[oride[0][3]]))
         # Conflict Active
-        elif len(list(filter(lambda x: x['state'] in {'war', 'election', 'civilwar'},sys['active_states']))) > 0:
-            thisconflict = sys["conflicts"][0]  
+        elif len(list(filter(lambda x: x['state'] in {'war', 'election', 'civilwar'}, sys['active_states']))) > 0:
+            thisconflict = sys["conflicts"][0]
             messages.append(amessage(
                 sys, 2, '{3} against {0} ({1} v {2})'.format(
-                    thisconflict["opponent_name"], 
-                    thisconflict["days_won"], dayslost(sys["system_name"],thisconflict["opponent_name"]), 
+                    thisconflict["opponent_name"],
+                    thisconflict["days_won"], dayslost(
+                        sys["system_name"], thisconflict["opponent_name"]),
                     thisconflict["type"].title()),
                 dIcons[thisconflict["type"]],
-                ))
+            ))
         # Conflict Pending
-        elif len(list(filter(lambda x: x['state'] in {'war', 'election', 'civilwar'},sys['pending_states']))) > 0:
-            thisconflict = sys["conflicts"][0]  
+        elif len(list(filter(lambda x: x['state'] in {'war', 'election', 'civilwar'}, sys['pending_states']))) > 0:
+            thisconflict = sys["conflicts"][0]
             messages.append(amessage(
                 sys, 2, '{1} Pending with {0}'.format(
-                    thisconflict["opponent_name"], 
+                    thisconflict["opponent_name"],
                     thisconflict["type"].title()),
                 dIcons[thisconflict["type"]],
-                ))
+            ))
         # Push  conrol
-        elif inf[0]['name'] != factionname:  
+        elif inf[0]['name'] != factionname:
             messages.append(
                 amessage(sys, 3, f'Boost {factionname} for system control', dIcons['push']))
         # Gap to 2nd place is low
-        elif inf[0]['influence']-inf[1]['influence'] < M_INFGAP:  
+        elif inf[0]['influence']-inf[1]['influence'] < M_INFGAP:
             messages.append(
                 amessage(sys, 4, f'Boost {factionname} - {inf[1]["name"]} is threatening ({inf[0]["influence"]-inf[1]["influence"]:4.3}%)', dIcons['infgap']))
         # Inf is too low
-        elif inf[0]['influence'] < M_MININF:  
+        elif inf[0]['influence'] < M_MININF:
             messages.append(amessage(
                 sys, 5, f'Boost {factionname} - vulnerable at {inf[0]["influence"]:4.3}%', dIcons['mininf']))
         # Data out of date
-        elif age.days > (inf[0]['influence']/10):  
+        elif age.days > (inf[0]['influence']/10):
             messages.append(
                 amessage(sys, 11, f'Scan System to update data {int(age.days)} days old', dIcons['data']))
 
         # Conflict Complete Info - Additional Message, not for Patrol, but for Discord
-        if len(list(filter(lambda x: x['state'] in {'war', 'election', 'civilwar'},sys['recovering_states']))) > 0:
+        if len(list(filter(lambda x: x['state'] in {'war', 'election', 'civilwar'}, sys['recovering_states']))) > 0:
             thisconflict = sys["conflicts"][0]
-            if thisconflict["days_won"] == dayslost(sys["system_name"],thisconflict["opponent_name"]):
+            if thisconflict["days_won"] == dayslost(sys["system_name"], thisconflict["opponent_name"]):
                 # Draw
-                asset=''
-            elif  thisconflict["days_won"]>dayslost(sys["system_name"],thisconflict["opponent_name"]):
+                asset = ''
+            elif thisconflict["days_won"] > dayslost(sys["system_name"], thisconflict["opponent_name"]):
                 # Won
-                asset = assetatrisk(sys["system_name"],thisconflict["opponent_name"])
+                asset = assetatrisk(sys["system_name"],
+                                    thisconflict["opponent_name"])
                 if asset != '':
-                    asset = 'Gained '+ asset
+                    asset = 'Gained ' + asset
             else:
                 # Lost
                 asset = thisconflict["stake"]
                 if asset != '':
-                    asset = 'Lost '+ asset
+                    asset = 'Lost ' + asset
 
             messages.append(amessage(
                 sys, 21, '{3} against {0} Complete ({1} v {2}) {4}'.format(
-                    thisconflict["opponent_name"], 
-                    thisconflict["days_won"], dayslost(sys["system_name"],thisconflict["opponent_name"]), 
+                    thisconflict["opponent_name"],
+                    thisconflict["days_won"], dayslost(
+                        sys["system_name"], thisconflict["opponent_name"]),
                     thisconflict["type"].title(),
                     asset),
                 dIcons["info"]))
-
 
         # Record All States for Usefull Summary Information
         for x in sys['active_states']:
@@ -255,8 +264,8 @@ def Misson_Gen(argv):
         if sum(x["system_name"] == ex[0] for x in faction_systems) == 0:
             exsys = external_system(ex[0])
             try:
-                messages.append(amessage(exsys, ex[1], ex[2]+'*', 
-                dIcons['override'] if ex[3] == '' else dIcons[ex[3]]))                
+                messages.append(amessage(exsys, ex[1], ex[2]+'*',
+                                         dIcons['override'] if ex[3] == '' else dIcons[ex[3]]))
             except:
                 print(f'!Override Ignored : {ex[0]} {ex[2]}')
     messages.sort()
@@ -264,16 +273,19 @@ def Misson_Gen(argv):
     # Looks for changes in Discord suitable Messages since last run for WebHook
     messagechanges = []
     for x in messages:
-        if x not in oldmessage:
+        if (x not in oldmessage) or ('/new' in argv):
             messagechanges.append(x)
     # Looks to see what systems no longer have a message of any type
     for x in oldmessage:
         s = list(filter(lambda y: y[1] == x[1], messages))
         if len(s) == 0:
             messagechanges.append(x)
-            messagechanges[len(messagechanges)-1][7] += ' : Misson Complete'
-            messagechanges[len(messagechanges)-1][8] = dIcons['end']
-    
+            messagechanges[len(messagechanges) - 1][7] += ' : Misson Complete'
+            messagechanges[len(messagechanges) - 1][8] = dIcons['end']
+
+    for m in messages:
+        print(f'{m[1]} : {m[7]}')
+
     # return(None)
 
     # Write Orders various formats
@@ -283,55 +295,59 @@ def Misson_Gen(argv):
             f'{x[1]},{x[2]},{x[3]},{x[4]},{x[5]},{x[7]}\n' for x in messages)
     with open(f'{factionname}DiscordPatrol.txt', 'w') as io:  # Text Version for Discord
         io.writelines(f'Canonn System Numberwang\n')
-        io.writelines(f'{x[8]}{x[1]} : {x[7]}\n' for x in filter(lambda x: x[0]<11 or x[0]>20, messages))
+        io.writelines(f'{x[8]}{x[1]} : {x[7]}\n' for x in filter(
+            lambda x: x[0] < 11 or x[0] > 20, messages))
     with open(f'{factionname}DiscordWebhook.txt', 'w') as io:  # Webhook Text Version for Discord
-        io.writelines(f'{x[8]}{x[1]} : {x[7]}\n' for x in filter(lambda x: x[0]<11 or x[0]>20, messagechanges))
+        io.writelines(f'{x[8]}{x[1]} : {x[7]}\n' for x in filter(
+            lambda x: x[0] < 11 or x[0] > 20, messagechanges))
     with open(f'{factionname}Message.json', 'w') as io:
-        json.dump(messages,io, indent=4)
-
+        json.dump(messages, io, indent=4)
 
     # Discord Webhook
-    if len(list(filter(lambda x: x[0]<11 or x[0]>20, messagechanges)))>0 and CSNSettings.wh_id != '':
+    if len(list(filter(lambda x: x[0] < 11 or x[0] > 20, messagechanges))) > 0 and CSNSettings.wh_id != '':
         wh_text = ''
-        wh = Webhook.partial(CSNSettings.wh_id, CSNSettings.wh_token,\
-            adapter = RequestsWebhookAdapter())
-        for x in filter(lambda x: x[0]<11 or x[0]>20, messagechanges):
-            wh_text+=f'{x[8]}{x[1]} : {x[7]}\n' 
+        wh = Webhook.partial(CSNSettings.wh_id, CSNSettings.wh_token,
+                             adapter=RequestsWebhookAdapter())
+        for x in filter(lambda x: x[0] < 11 or x[0] > 20, messagechanges):
+            wh_text += f'{x[8]}{x[1]} : {x[7]}\n'
         if wh_text != '':
-            wh.send(f'<:canonn:231835416548999168> \n{wh_text}')
+            wh.send(
+                f'{"Full Report" if ("/new" in argv) else "Latest News"} <:canonn:231835416548999168> \n{wh_text}')
 
     # Patrol to send to Google
-    patrol=[]
-    for x in filter(lambda x : x[0]<=20, messages):
+    patrol = []
+    for x in filter(lambda x: x[0] <= 20, messages):
         patrol.append(x[1:8])
     if not('/safe' in argv):
         CSNPatrolWrite(patrol)
-
-    for m in messages:
-        print(f'{m[1]} : {m[7]}')
-
 
     print('*** Missions Generated : Consuming {0} requests ***'.format(NREQ))
     if ('/wait') in argv:
         input("Press Enter to continue...")
 
+
 def amessage(sys, p, message, icon=''):
     return([p, sys["system_name"], sys["x"], sys["y"], sys["z"], 0, factionname, message, icon])
+
 
 def dayslost(system_name, faction):
     try:
         return(list(filter(lambda x: x["system_name"] == system_name, getfactiondata(faction)))[0]["conflicts"][0]["days_won"])
     except:
         return(0)
+
+
 def assetatrisk(system_name, faction):
     try:
         return(list(filter(lambda x: x["system_name"] == system_name, getfactiondata(faction)))[0]["conflicts"][0]["stake"])
     except:
         return('')
 
+
 def EliteBGSDateTime(s):
     dformat = '%Y-%m-%dT%H:%M:%S'  # so much grief from this function
-    return(datetime.strptime(s[:len(dformat)+2], dformat))  
-        
+    return(datetime.strptime(s[:len(dformat) + 2], dformat))
+
+
 if __name__ == '__main__':
-    Misson_Gen(sys.argv[1:]+["/Test2","/Test3"])
+    Misson_Gen(sys.argv[1:] + ["/Test2", "/Test3"])
