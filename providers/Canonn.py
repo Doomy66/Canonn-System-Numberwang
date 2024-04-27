cb55a3125fc5de1f0f4f0752d723fcb54ddf02c7
Steve Burrows <steven.burrows@hexagon.com>
2024-04-27 14:45:24 +0100

    fleet carrier api endpoint changed

--- providers/Canonn.py
+++ providers/Canonn.py
@@ -1,20 +1,24 @@
 # Canonn Research - Fleet Carrier Location
-from CSNSettings import CSNLog
 import requests
 import json
 
-_CANONN = 'https://us-central1-canonn-api-236217.cloudfunctions.net/'
+_CANONN = 'https://us-central1-canonn-api-236217.cloudfunctions.net/query/'
 
 
 def getfleetcarrier(fc_id):
     """ Get FC Info from Canonn API """
     try:
-        url = f"{_CANONN}postFleetCarriers"
-        payload = {'serial': fc_id}
-        resp = requests.get(url, params=payload)
+        # url = f"{_CANONN}postFleetCarriers"
+        # payload = {'serial': fc_id}
+        url = f"{_CANONN}fleetCarrier/{fc_id}"
+        resp = requests.get(url)
         myload = json.loads(resp._content)[0]
     except:
-        CSNLog.info(f'Failed to find FC "{fc_id}"')
+        # CSNLog.info(f'Failed to find FC "{fc_id}"')
         print(f'!Failed to find FC "{fc_id}"')
         myload = None
     return myload
+
+
+if __name__ == '__main__':
+    print(getfleetcarrier('TNY-09Z'))
